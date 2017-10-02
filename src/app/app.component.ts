35e65d0e5e65273a49cb3454df4ac5d40c1efc5d
Lemuel Dizon <lemuel.dizon.703@my.csun.edu>
2017-10-01 19:04:36 -0700

    Added MyEvents page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { MyEventsPage } from '../pages/my-events/my-events';
 
 @Component({
   templateUrl: 'app.html'
 })
+
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
@@ -22,7 +24,7 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'My Events', component: MyEventsPage }
     ];
 
   }
